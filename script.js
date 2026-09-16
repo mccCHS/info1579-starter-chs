c63a2dedbac80f106c7f3a391f219382c22c955a
mccCHS <csedlacek@mail.mccneb.edu>
2026-09-16 19:06:24 +0000

    todos completed

--- script.js
+++ script.js
@@ -52,4 +52,3 @@ display("Daily Study Hours (with rest day)", adjustedDailyHours.toFixed(2));
 display("Daily Study Minutes (with rest day)", `${(adjustedDailyHours * 60).toFixed(2)} minutes`);
 display("Percent Complete", `${percentComplete.toFixed(2)}%`);
 display("Percent Remaining", `${percentRemaining.toFixed(2)}%`);
-// test
